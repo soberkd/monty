1e7f0685a151d8a71a99bc1e0b6dd9e9a201b76c
soberkd <soberkd6@gmail.com>
2022-12-15 15:03:28 +0000

    Print values in stack, from top

---
+++ _pall.c
@@ -0,0 +1,22 @@
+#define DEFINE_VARIABLES
+#include "monty.h"
+#include "extern.h"
+
+/**
+  * _pall - prints all the values on the stack, starting
+  *          from the top of the stack.
+  * @stack: the monty stack.
+  * @line_number: opcode index.
+  */
+
+void _pall(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
+{
+	stack_t *process;
+
+	process = *stack;
+	while (process != NULL)
+	{
+		printf("%d\n", process->n);
+		process = process->next;
+	}
+}
