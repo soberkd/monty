80666b9f4745a0b2cbe2623a14e5a0e210f577e6
soberkd <soberkd6@gmail.com>
2022-12-15 15:27:10 +0000

    Print the value at the top of stack

---
+++ _pint.c
@@ -0,0 +1,24 @@
+#define DEFINE_VARIABLES
+#include "monty.h"
+#include "extern.h"
+
+/**
+  * _pint - prints the value at the top of the stack,
+  *           followed by a new line.
+  * @stack: monty stack
+  * @line_number: opcode index.
+  *
+  */
+
+void _pint(stack_t **stack, unsigned int line_number)
+{
+	stack_t *process;
+
+	process = *stack;
+	if (process == NULL)
+	{
+		printf("L%d: can't pint, stack empty\n", line_number);
+		error_exit(stack);
+	}
+	printf("%d\n", process->n);
+}
