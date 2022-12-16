ae45e3c6e661ce1043bb392ec99f31193d5718a4
soberkd <soberkd6@gmail.com>
2022-12-16 12:10:38 +0000

    Compute remainder od division elements

--- _mod.c
+++ _mod.c
@@ -24,7 +24,7 @@ void _mod(stack_t **head, unsigned int counter)
 		fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
 		fclose(bus.file);
 		free(bus.content);
-		free_stack(*head);
+		free_dlistint(*head);
 		exit(EXIT_FAILURE);
 	}
 	h = *head;
