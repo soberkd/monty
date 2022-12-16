77c0fdc940661bc59bbd24263c33faebd5913177
soberkd <soberkd6@gmail.com>
2022-12-16 11:35:44 +0000

    Add two elements of a stack

--- _add.c
+++ _add.c
@@ -1,19 +1,35 @@
 #include "monty.h"
 
 /**
-  * _add - adds the top two elements of the stack.
-  * @stack: pointer to stack pointer
-  * @line_number: the line index of the opcode
-  *
-  */
+ * _add - adds the top two elements of the stack.
+ * @stack: pointer to stack pointer
+ * @line_number: the line index of the opcode
+ *
+ */
 
 void _add(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL || (*stack)->next == NULL)
+	stack_t *h;
+	int length = 0, temp;
+
+	h = *stack;
+	while (h)
 	{
-		printf("L%d: can't add, stack too short\n", line_number);
-		error_exit(stack);
+		h = h->next;
+		length++;
 	}
-	(*stack)->next->n += (*stack)->n;
-	_pop(stack, line_number);
+	if (length < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n"
+				, line_number);
+		fclose(bus.file);
+		free(bus.content);
+		free_dlistint(*stack);
+		exit(EXIT_FAILURE);
+	}
+	h = *stack;
+	temp = h->n + h->next->n;
+	h->next->n = temp;
+	*stack = h->next;
+	free(h);
 }
